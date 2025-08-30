ba3a9b60a62fc9b945b531366f726ebf164ceb49
KENDIA06 <kevinaradia@gmail.com>
2025-08-30 08:22:24 +0700

    hasil.py

--- hasil.py
+++ hasil.py
@@ -308,8 +308,8 @@ def interactive_sentiment_test_nb():
     col_info1, col_info2 = st.columns(2)
     with col_info1:
         if model_accuracy:
-          if model_accuracy is not None:
-    st.info(f"Model Accuracy: {model_accuracy:.2%}")
+         if model_accuracy is not None:
+        st.info(f"Model Accuracy: {model_accuracy:.2%}")
 else:
     st.info("Model Accuracy: Not available")
         else:
@@ -1399,6 +1399,7 @@ elif selected == "Distribusi":
         
     else:
         st.error("Kolom 'Label' tidak ditemukan dalam data")
+
 
 
 
